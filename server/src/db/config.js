a063ad16734ff3bf6ec9a3c0f434886e12c444e4
BrandonKing6 <kingbrandon1726@gmail.com>
2024-11-21 18:40:25 +0000

    Add files via upload

--- server/src/db/config.js
+++ server/src/db/config.js
@@ -1,1 +1,10 @@
-// add your database connection here
+const { Sequelize } = require('sequelize');
+
+// Create sequelize instance
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: './database.sqlite',
+  logging: false,
+});
+
+module.exports = sequelize; 
